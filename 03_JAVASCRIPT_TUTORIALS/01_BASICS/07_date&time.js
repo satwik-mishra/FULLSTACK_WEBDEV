f4fab3c5c9e15572542ce721a93d0a9dad3382dc
satwik mishra <147480693+satwik-mishra@users.noreply.github.com>
2025-09-15 23:09:08 +0530

    removed 01_basics

--- 03_JAVASCRIPT_TUTORIALS/01_BASICS/07_date&time.js
+++
@@ -1,6 +0,0 @@
-let mydate = new Date()
-console.log(mydate.toString());
-console.log(mydate.toISOString());
-console.log(mydate.toDateString);
-console.log(mydate.toLocaleString());
-console.log(typeof(mydate));
