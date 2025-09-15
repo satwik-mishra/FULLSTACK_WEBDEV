4c2f8a88a26e5af859d4cf5e28972b78d2c0a28d
satwik mishra <147480693+satwik-mishra@users.noreply.github.com>
2025-09-15 23:34:09 +0530

    01_helloworld

--- 03_JAVASCRIPT_TUTORIALS/01_helloworld.js
+++ 03_JAVASCRIPT_TUTORIALS/01_helloworld.js
@@ -1,1 +1,15 @@
 console.log("hello world, I am satwik");
+console.log("i like pizza");
+
+window.alert(`this is an alert!`)
+
+// comments : this is a comment 
+/* this 
+    is
+    a 
+    multiline 
+    comment 
+*/
+document.getElementById("myH1").textContent="hello world i am satwik";
+document.getElementById("myP").textContent="i like pizza and burger"
+
